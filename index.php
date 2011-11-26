a419be4f742bc3a0edb4bcdf1a0b12c18cb50ca3
Jack Polgar <jack@polgar.id.au>
2011-11-26 15:42:00 +1100

    Moved path define code to paths.php to DRY up the index.php and oil files.

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
-/**
- * Website document root
- */
-define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
-
-/**
- * Path to the application directory.
- */
-define('APPPATH', __DIR__.'/system/'.DIRECTORY_SEPARATOR);
-
-/**
- * Path to the default packages directory.
- */
-define('PKGPATH', __DIR__.'/system/packages/'.DIRECTORY_SEPARATOR);
-
-/**
- * The path to the framework core.
- */
-define('COREPATH', __DIR__.'/system/fuel/'.DIRECTORY_SEPARATOR);
+require "paths.php";
 
 // Get the start time and memory for use later
 defined('FUEL_START_TIME') or define('FUEL_START_TIME', microtime(true));
---
+++ paths.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Website document root
+ */
+define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
+
+/**
+ * Path to the application directory.
+ */
+define('APPPATH', __DIR__.'/system/'.DIRECTORY_SEPARATOR);
+
+/**
+ * Path to the default packages directory.
+ */
+define('PKGPATH', __DIR__.'/system/packages/'.DIRECTORY_SEPARATOR);
+
+/**
+ * The path to the framework core.
+ */
+define('COREPATH', __DIR__.'/system/fuel/'.DIRECTORY_SEPARATOR);
