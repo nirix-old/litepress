10f6d88b65e40ac7feb2de76d296dbc3a3bf8185
Jack Polgar <jack@polgar.id.au>
2011-11-27 04:06:08 +1100

    Fix the path defining.

--- paths.php
+++ paths.php
@@ -7,14 +7,14 @@ define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
 /**
  * Path to the application directory.
  */
-define('APPPATH', __DIR__.'/system/'.DIRECTORY_SEPARATOR);
+define('APPPATH', __DIR__.'/system'.DIRECTORY_SEPARATOR);
 
 /**
  * Path to the default packages directory.
  */
-define('PKGPATH', __DIR__.'/system/packages/'.DIRECTORY_SEPARATOR);
+define('PKGPATH', __DIR__.'/system/packages'.DIRECTORY_SEPARATOR);
 
 /**
  * The path to the framework core.
  */
-define('COREPATH', __DIR__.'/system/fuel/'.DIRECTORY_SEPARATOR);
+define('COREPATH', __DIR__.'/system/fuel'.DIRECTORY_SEPARATOR);
